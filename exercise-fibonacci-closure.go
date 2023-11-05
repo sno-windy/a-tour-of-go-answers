0b93e81d4511931d1a87740d03cfcf00454727ac
higaki sota <79880317+sno-windy@users.noreply.github.com>
2023-11-05 18:20:19 +0900

    Create exercise-fibonacci-closure.go

---
+++ exercise-fibonacci-closure.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	fib1, fib2 := 0, 1
+	return func() int {
+		// tmp := fib1
+		// fib1 = fib2
+		// fib2 = tmp + fib2
+		// return tmp
+		f := fib1
+		fib1, fib2 = fib2, fib1 + fib2
+		return f
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
